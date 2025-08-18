18748912ede386198f5fd2270209aede734aa357
Madhan <madhan.kmr@outlook.com>
2025-08-19 00:59:07 +0300

    Add files via upload

---
+++ Models/APIConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace Models
+{
+    public class APIConfiguration
+    {
+        public string BaseUrl { get; set; } = string.Empty;
+        public string ClientID { get; set; } = string.Empty;
+        public string ClientSecret { get; set; } = string.Empty;
+    }
+}
